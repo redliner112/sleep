509c7ca638c6d28de2ae4020da8de5f8bd112358
DESKTOP-33RS5H4\redmi <redmiler@naver.com>
2018-06-13 00:45:26 +0900

    initial commit

---
+++ src/main/java/net/sleep/SleepApplication.java
@@ -0,0 +1,12 @@
+package net.sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepApplication.class, args);
+	}
+}
